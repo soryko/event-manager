7026f34d4f0efb26024806daac60999382ef465a
soryko <sorykora@gmail.com>
2024-04-28 10:17:51 +0200

    added phone number cleaning and popular times

--- lib/event-manager.rb
+++ lib/event-manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -10,7 +11,7 @@ def legislators_by_zipcode(zip)
       address: zip,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
+    ).officials
     legislators = legislators.officials
     legislator_names = legislators.map(&:name)
     legislator_names.join(", ")
@@ -23,6 +24,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone)
+  phone = phone.to_s.delete("/\D/")
+  if phone.length == 10
+    return phone 
+  end
+  if phone.to_s.length == 11 && phone[0] == 1
+    return phone[1..-1]
+  end
+  return "Invalid Number"
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -31,9 +43,34 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+hours = []
+days = []
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
+  hour = Time.strptime(row[:regdate], "%m/%d/%Y %k:%M").hour
+  hours.push(hour)
+  day_week = Time.strptime(row[:regdate], "%m/%d/%Y %k:%M").wday
+  days.push(day_week)
+  phone = clean_phone_number(row[:homephone])
   legislators = legislators_by_zipcode(zipcode)
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id,form_letter)
 end
+
+puts "popular hours: #{hours.tally}"
+puts "popular days: #{days.tally}"
+
